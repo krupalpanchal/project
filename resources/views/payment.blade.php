32c7c45c3ceade7f480445ffae17a941bac14541
Krupal panchal <krupal.p@inheritx.com>
2026-04-01 18:03:45 +0530

    second push

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -19,7 +19,7 @@
                     {{ session('error') }}
                 </div>
             @endif
-            <h1>Hello change</h1>
+            <h1>Hello change dev</h1>
             <form action="{{ route('payment.checkout') }}" method="POST" class="space-y-4">
                 @csrf
                 <div>
